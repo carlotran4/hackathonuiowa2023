8a441ac527445e2c9f1d4f7e7c0b8b09d7532c2e
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2023-09-23 16:12:22 -0500

    making progress

--- javascript/beat.js
+++ javascript/beat.js
@@ -1,32 +1,39 @@
 
 function initButtons(){
-    var instruments = 8;
+    var instruments = ["yuh","cymbals","snare","kickdrum"];
     var beats = 8;
     var squaresPerBeat = 4;
 
     var div = document.getElementById('beatCheckboxes');
-    for(i = 0; i<instruments;i++){
+    for(i = 0; i<instruments.length;i++){
         var instrumentDiv = document.createElement('div');
         div.appendChild(instrumentDiv);
         instrumentDiv.id = "instrument-"+i;
         instrumentDiv.className="instrument"
+        var instrumentButton = document.createElement("button");
+        var instrument = document.createElement("img")
+        instrument.setAttribute("src","\\images\\"+instruments[i]+".png");
+        instrument.id=instrumentDiv.id+"InstrumentLogo";
+        instrumentDiv.appendChild(instrumentButton);
+        instrumentButton.appendChild(instrument)
+
         for(j=0;j<beats*squaresPerBeat;j++){
-            var container = document.createElement('div');
-            container.classList.add('checkBoxContainer')
-            var checkbox = document.createElement('input');
-            checkbox.type='checkbox';
-            checkbox.classList.add("beatCheckbox");
-            if(j%squaresPerBeat==0){
-                checkbox.classList.add('sigBeatbox')
-            }
-            checkbox.id=instrumentDiv.id+"-"+j;
-            var checkmark = document.createElement('span');
-            checkmark.classList.add("boxCheck");
-            instrumentDiv.appendChild(container);
-            container.appendChild(checkbox);
-            container.appendChild(checkmark);
+            var checkbox = document.createElement('div');
+            checkbox.id = "box-"+i+"-"+j;
+            checkbox.className="beatBox";
+            instrumentDiv.appendChild(checkbox);
         }
     }
 }
 
+
 initButtons();
+
+var buttons = document.getElementsByClassName("beatBox");
+for(let button of buttons){
+    console.log(button.id)
+    button.addEventListener("click",function(){
+        console.log("clicked");
+        this.classList.toggle("clicked");
+    })
+}
